74e7cc50c3b54913087fa6600460fdfbc7bdffe5
arkan0303 <arkanuladelis8@gmail.com>
2023-10-17 22:12:38 +0700

    mana

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { count, time } = require("console");
 const express = require ("express")
 const path = require ("path");
 const { title } = require("process");
@@ -17,9 +18,10 @@ app.use(express.urlencoded({extended: false}))
 app.get("/home",home)
 app.get("/upload-project", project)
 app.post("/upload-project", submit) 
+app.get("/delete-project/:id", hapus )
 app.get("/testimonial",testi)
 app.get("/contact", contact)
-app.get("/detail-post", post)
+app.get("/detail-post/:id", post)
 
 
 
@@ -46,7 +48,8 @@ const projects =[
         author : "Arkanul Adelis",
         Description : "Some quick example text to build on the card title and make up the bulk of the card's content",
         Technologies : ["node js", "next js", "react", "type script"]
-    }
+        
+    },
 ]
 function home (req, res){
     res.render("index", {projects});
@@ -55,37 +58,119 @@ function project (req, res){
     res.render("upload-project")
 }
 function testi (req, res){
-    res.render("testimonial")
+    res.render ("testimonial")
 }
 function contact (req, res){
     res.render("kontak")
 }
 function post (req, res){
-    res.render("project-detail")
+    const {id} = req.params
+    console.log(id);
+    res.render("project-detail", {projects})
 }
 
 function submit (req, res){
-    let run={
-        "title": Title,
-        "start-date": startDAte,
+    let {
+        "title": title,
+        "start-date" : startDate,
         "end-date": endDate,
-        Technologies,
-        Description,
+        "Technologies": Technologies,
+        "Description":Description,
     } = req.body;
-    console.log(req.body);
-    console.log(run);
+    console.log();
+    console.log("title", "startDate", "endDate", "Technologies", "Description");
 
-    const data = {
-        "title": Title,
-        "start-date": new Date(),
-        "end-date": endDate,
-        Technologies,
-        Description,
-        author : "Arkanul Adelis"
+    //durasi
+    let startDateValue =new Date (startDate);
+    let endDateValue = new Date(endDate);
+
+    let durasiWaktu = endDateValue.getTime() - startDateValue.getTime();
+    let durasiHari = durasiWaktu / ( 1000 * 3600 * 24);
+    let durasiMinggu = Math.floor(durasiHari / 7 );
+    let durasiBulan = Math.floor (durasiHari / 30);
+    let durasiProject = "";
+
+    if (durasiHari <= 6){
+        durasiProject = "Durasi" + " " + durasiHari + " " + "Hari";
+    }else if (durasiMinggu <= 3){
+        durasiProject = "Durasi" + " " + durasiMinggu + " " + "Minggu";
+    }else if ( durasiBulan >= 1){
+        durasiProject ="Durasi" + " " + durasiBulan + " " + "Bulan" ;
     }
 
-    projects.push(data)
+     //icon
+     const nodeJs = `<img src="/image/node.png" alt="" />`;
+     const nextJs = `<img src="/image/nextjs.png" alt="" />`;
+     const reactJs = ` <img src="/image/react.png" alt="" />`;
+     const typeScript = `<img src="/image/type.png" alt="" />`;
+ 
+ 
+ 
+ 
+     //cek kondisi
+    //  let getIcon1 = Technologies.checked
+    //   ? nodeJs : "";
+    //  let getIcon2 = Technologies.checked
+    //   ? nextJs : "";
+ 
+    //  let getIcon3 = Technologies.checked 
+    //  ? reactJs : "";
+    //  let getIcon4 =Technologies.checked
+    //  ? typeScript : "";
+
+    const data = {
+        title,
+        startDate,
+        endDate,
+        durasiProject,
+        Description,
+        // getIcon1,
+        // getIcon2,
+        // getIcon3,
+        // getIcon4,
+        postAt : new Date(),
+        author : "Arkanul Adelis",
+    }
+
+    projects.unshift(data)
     console.log(projects);
+    console.log(req.body);
 
     res.redirect("/home")
 };
+
+function hapus (req, res){
+    const { id } = req.params
+    projects.splice(id, 1);
+    res.redirect("/home")
+    console.log(id);
+    
+}
+
+
+//edit projet
+app.get("/project/edit/:id", (req, res) => {
+    const { id } = req.params
+    const blog = projects.filter((blog) => blog.id == id)[0]
+  
+    res.render("edit-project", { layout: "edit-project", active: { Blog: true }, blog })
+  })
+
+//   router.post("/project/edit/:id", (req, res) => {
+//     const { id } = req.params
+//     const { title, content, image } = req.body
+//     projects[id] = {
+//       id: id,
+//       title,
+//       startDate,
+//       enddate,
+//       durasiProject,
+//       Description,
+//       Technologies,
+//       postedAt: moment().format("Do MMM YY"),
+//       author: "Patrick",
+//     }
+  
+//     console.log(blogs)
+//     res.redirect("/home")
+//   })
