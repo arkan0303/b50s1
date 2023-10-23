ba32bc4dd4d1ffe2df497dd394e51998a395e818
arkan0303 <arkanuladelis8@gmail.com>
2023-10-23 09:42:06 +0700

    t

--- src/migrations/20231019184236-create-project.js
+++ src/migrations/20231019184236-create-project.js
@@ -1,46 +1,53 @@
-'use strict';
+"use strict";
+
+const { Model } = require("sequelize");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Projects', {
+    await queryInterface.createTable("Projects", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       start_date: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       end_date: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       technologies: {
-        type: Sequelize.ARRAY(Sequelize.STRING)
+        type: Sequelize.ARRAY(Sequelize.STRING),
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       author: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Projects');
-  }
+    await queryInterface.dropTable("Projects");
+  },
 };
